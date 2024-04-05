d4dcfdb77e521c4f92b37f427dfce33ac54984ef
Chris Buyck <cabuyck@gmail.com>
2024-04-05 01:08:48 -0500

    Initial commit

---
+++ test/adventure-crew-guides-infrastructure.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AdventureCrewGuidesInfrastructure from '../lib/adventure-crew-guides-infrastructure-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/adventure-crew-guides-infrastructure-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AdventureCrewGuidesInfrastructure.AdventureCrewGuidesInfrastructureStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
